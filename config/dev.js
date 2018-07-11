7b80be72b6559eb91e764ce5bf99d737d1bf7fc6
Aaron Ely <aaron.ely@hotmail.com>
2018-07-10 19:52:00 -0700

    finished auth flow

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '1009827668717-3p3kvl3dpc6e4ss6d6lbdgg9e6fpm32t.apps.googleusercontent.com',
+    googleClientSecret: 'mOX3vl912sUywJTqoyRU1H9S',
+    mongoURI: 'mongodb://aaron:password1@ds231501.mlab.com:31501/emaily-dev',
+    cookieKey: 'alkdjfanovibavnakjvoijaoinoinoigrnoiavlaknv'
+};
