b5aa94a97101a969b92eca927ff929280f47354a
Aaron Ely <aaron.ely@hotmail.com>
2018-07-16 18:44:55 -0700

    Added billing and client side app!

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -24,7 +24,7 @@ class Header extends Component {
         return (
             <div>
                 <nav>
-                    <div className="navbar">
+                    <div className="nav-wrapper">
                     <Link to={this.props.auth ? '/surveys' : '/'} className="left brand-logo">Emaily</Link>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
                         {this.renderContent()}
